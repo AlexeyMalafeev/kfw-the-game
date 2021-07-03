50bd2704c8c3299a1f931a378e8ea179dc8dd57e
AlexeyMalafeev <amalafeev@yandex.ru>
2021-07-03 20:14:00 +0300

    refactor: fighter._constants

--- kf_lib/actors/fighter/__init__.py
+++ kf_lib/actors/fighter/__init__.py
@@ -7,77 +7,7 @@ from ...things import weapons
 from ...utils import exceptions
 from ...utils.utilities import *
 
-
-ADVANCED_TECH_AT_LV = 20
-BLOCK_DIVISOR = 2
-COUNTER_CHANCE_BASE = 0.1
-COUNTER_CHANCE_INCR_PER_LV = 0.02
-DAM_DIVISOR = 2
-DODGE_DIVISOR = 3
-DUR_LYING_MIN = 100
-DUR_LYING_MAX = 200
-DUR_OFF_BAL_MIN = 30
-DUR_OFF_BAL_MAX = 60
-DUR_SHOCK_MIN = 50
-DUR_SHOCK_MAX = 100
-DUR_SLOW_MIN = 300
-DUR_SLOW_MAX = 600
-DUR_STUN_MIN = 50
-DUR_STUN_MAX = 150
-FALL_DAMAGE = (25, 50)
-HP_PER_HEALTH_LV = 50
-INSTA_KO_CHANCE = 0.25
-KNOCKBACK_DIST_FORCED = (1, 1, 1, 2, 2, 3)
-KNOCKBACK_HP_THRESHOLDS = (0.3, 0.35, 0.4)  # correspond to levels of knockback: 1, 2, 3
-KNOCKDOWN_HP_DIVISOR = 2     # todo make 0.5 like above thresholds
-LEVEL_BASED_DAM_UPPER_MULT = 10  # * self.level in damage; upper bound
-LVS_GET_NEW_ADVANCED_MOVE = {10, 12, 14, 16, 18, 20}  # should be ordered, ascending
-NEW_MOVE_TIERS = {
-    1: 1,
-    2: 1,
-    3: 2,
-    4: 2,
-    5: 3,
-    6: 3,
-    7: 4,
-    8: 4,
-    9: 5,
-    10: 5,
-    11: 6,
-    12: 6,
-    13: 7,
-    14: 7,
-    15: 8,
-    16: 8,
-    17: 9,
-    18: 9,
-    19: 10,
-    20: 10,
-}
-LVS_GET_NEW_TECH = {11, 13, 15, 17, 19}
-MOB_DAM_PENALTY = 0.3
-OFF_BALANCE_HP_DIVISOR = 4  # todo make 0.25 like above thresholds
-QI_BASED_DAM_UPPER_MULT = 3
-QP_BASE = 0
-QP_INCR_PER_LV = 5
-RATIO_NO_RISK = 0
-RATIO_VERY_LOW_RISK = 0.5
-RATIO_LOW_RISK = 0.8
-RATIO_FAIR_FIGHT = 0.95
-RATIO_RISKY = 1.1
-RATIO_VERY_RISKY = 1.5
-RATIO_EXTREMELY_RISKY = 2
-SHOCK_CHANCE = 0.5  # for moves
-STAMINA_BASE = 50  # for all fighter levels
-STAMINA_DAMAGE = 0.2  # for moves
-STAMINA_FACTOR_BIAS = 0.5
-STAMINA_INCR_PER_LV = 10
-STAT_BASED_DAM_UPPER_MULT = 5
-STUN_HP_DIVISOR = 2.8
-TIME_UNIT_MULTIPLIER = 20
-
-INDENT = 0
-ALIGN = 60
+from ._constants import *
 
 
 class Fighter(object):
