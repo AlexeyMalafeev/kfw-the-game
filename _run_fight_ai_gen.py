69cb586c31001e0a688147621c655e768c2d9693
AlexeyMalafeev <amalafeev@yandex.ru>
2022-02-08 14:47:56 +0300

    feat: genetic algo tweaks

--- _run_fight_ai_gen.py
+++ _run_fight_ai_gen.py
@@ -1,26 +1,25 @@
 from kf_lib.ai import fight_ai, fight_ai_gen
 
 try:
-    # ga = fight_ai_gen.GeneticAlgorithm(
-    #     pop_size=16,
-    #     # n_fights_per_setting=16,
-    #     n_fights_per_setting=1,
-    #     gene_names=fight_ai.GENETIC_AI_PARAM_NAMES,
-    #     mutation_prob=0.1,
-    #     infighting=True,
-    # )
-    # ga.run(3)
-
     ga = fight_ai_gen.GeneticAlgorithm(
-        pop_size=64,
-        # n_fights_per_setting=64,
+        pop_size=16,
         n_fights_1on1=8,
         n_fights_crowd=2,
         gene_names=fight_ai.GENETIC_AI_PARAM_NAMES,
         mutation_prob=0.1,
+        infighting=True,
+    )
+    ga.run(128)
+
+    ga = fight_ai_gen.GeneticAlgorithm(
+        pop_size=32,
+        n_fights_1on1=64,
+        n_fights_crowd=16,
+        gene_names=fight_ai.GENETIC_AI_PARAM_NAMES,
+        mutation_prob=0.1,
         infighting=False,
     )
-    ga.run(3)
+    ga.run(128)
 
     input('Press Enter to exit')
 
