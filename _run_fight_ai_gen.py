03263dd08badbc42ef40dd16d179574fe06a0a61
AlexeyMalafeev <amalafeev@yandex.ru>
2022-02-08 12:08:46 +0300

    refactor: genetic algorithm for fight AI training

--- _run_fight_ai_gen.py
+++ _run_fight_ai_gen.py
@@ -1,21 +1,18 @@
 from kf_lib.ai import fight_ai, fight_ai_gen
 
 try:
-    # ga = fight_ai_gen.GeneticAlgorithm(pop_size=20, n_genes=5, gene_names=fight_ai.GENETIC_AI_PARAM_NAMES,
-    #                                    n_top=10, mut_prob=0.1)
     ga = fight_ai_gen.GeneticAlgorithm(
-        pop_size=32,
+        pop_size=64,
         gene_names=fight_ai.GENETIC_AI_PARAM_NAMES,
-        n_top=16,
         mut_prob=0.1,
         infighting=True,
         comment='',
     )
-    ga.run(30)
+    ga.run(100)
     input('Press Enter to exit')
 
 
-except Exception:
+except Exception: # noqa
     import traceback
     traceback.print_exc()
     input('Press Enter to exit')
--- kf_lib/ai/fight_ai_gen.py
+++ kf_lib/ai/fight_ai_gen.py
@@ -13,80 +13,62 @@ from kf_lib.ai import fight_ai, fight_ai_test
 from kf_lib.utils.utilities import rnd
 
 
-
-TRAINING_MODE: int = 1
-
-
 class GeneticAlgorithm(object):
     def __init__(
             self,
             pop_size: int,
             gene_names: List[str],
-            n_top: int,
             mut_prob: float,
             infighting: bool,
             comment: str = None
     ):
         """mode: if infighting is False, will train against current DefaultFightAI"""
+        assert pop_size % 2 == 0, 'pop_size must be divisible by 2'
         self.pop_size = pop_size
-        self.n_genes = len(gene_names)
         self.gene_names = gene_names
-        self.n_top = n_top
         self.mut_prob = mut_prob
+        self.fitness_function = self.fitness_infighting if infighting else self.fitness
         self.comment = comment
+
+        # setup
+        self.n_genes: int = len(self.gene_names)
+        self.n_top: int = self.pop_size // 2
         self.population = [[rnd() for _ in range(self.n_genes)] for _ in range(pop_size)]
         self.fit_values = []
         self.fit_values_sorted = []
         self.max_possible_fit_value = 0
         self.all_time_record = 0
         self.record_holder = None
+        self.record_generation = None
         self.fittest = []
         self.n_generations = 0
         self.curr_generation = 0
         self.mutations_occurred = 0
 
-    def crossover(
-        self,
-    ):  # todo remove doubles -> generate random individuals after a few failed crossover attempts
-        # print(time.ctime())
-        # print('starting crossover')
+    # todo remove doubles -> generate random individuals after a few failed crossover attempts
+    def crossover(self):
         new_population = self.fittest[:]
-        random.shuffle(
-            new_population
-        )  # to cancel out the effect of sorting when performing selection
+        # to cancel out the effect of sorting when performing selection
+        random.shuffle(new_population)
         for i in range(0, self.n_top, 2):
             parent_a = new_population[i]
             parent_b = new_population[i + 1]
-            # print('parents:')
-            # pprint(parent_a)
-            # pprint(parent_b)
             ind = list(range(self.n_genes))
             random.shuffle(ind)
-            ind = ind[: random.randint(1, len(ind) - 1)]
-            # print('crossing genes with indices:')
-            # pprint(ind)
+            ind = ind[:random.randint(1, len(ind) - 1)]
             child_a = parent_a[:]
             child_b = parent_b[:]
             for ii in ind:
                 child_a[ii], child_b[ii] = child_b[ii], child_a[ii]
             new_population.extend([child_a, child_b])
-            # print('children:')
-            # pprint(child_a)
-            # pprint(child_b)
         self.population = new_population
 
     def fitness(self):
         """Set self.fit_values"""
-        # print(time.ctime())
-        # print('starting fitness evaluation')
         self.fit_values = []
         n_rep = 250
         self.max_possible_fit_value = n_rep * 2
         for individual in self.population:
-            # print(time.ctime())
-            # print('evaluating individual:')
-            # pprint(individual)
-            # ai = fight_ai.GeneticAI
             ai = copy(fight_ai.DefaultGeneticAIforTraining)
             for i, name in enumerate(self.gene_names):
                 setattr(ai, name, individual[i])
@@ -98,28 +80,18 @@ class GeneticAlgorithm(object):
                 suppress_output=True,
             )
             self.fit_values.append(t.wins[0])
-        #     print('result:', t.wins[0])
-        # print('all fit values:')
-        # pprint(self.fit_values)
 
     def fitness_infighting(self):
         """Set self.fit_values"""
-        # print(time.ctime())
-        # print('starting fitness evaluation')
         self.fit_values = []
         n_rep = 10
         self.max_possible_fit_value = 2 * n_rep * len(self.population)
         for individual in self.population:
-            # print(time.ctime())
-            # print('evaluating individual:')
-            # pprint(individual)
             score = 0
-            # ai = fight_ai.GeneticAI
             ai = fight_ai.DefaultGeneticAIforTraining
             for i, name in enumerate(self.gene_names):
                 setattr(ai, name, individual[i])
             for individual2 in self.population:
-                # ai2 = fight_ai.GeneticAI2
                 ai2 = copy(fight_ai.DefaultGeneticAIforTraining)
                 for i, name in enumerate(self.gene_names):
                     setattr(ai2, name, individual2[i])
@@ -132,32 +104,15 @@ class GeneticAlgorithm(object):
                 )
                 score += t.wins[0]
             self.fit_values.append(score)
-        #     print('result:', score)
-        # print('all fit values:')
-        # pprint(self.fit_values)
 
     def mutation(self):
         if not self.mut_prob:
             return
-        # print(time.ctime())
-        # print('starting mutation')
-        # only the new children mutate to ensure preserving top fit values from the previous
-        # generation
         for individual in self.population[len(self.fittest):]:
             if rnd() <= self.mut_prob:
-                # print('mutation occurs for individual:')
-                # pprint(individual)
                 i = random.randint(0, self.n_genes - 1)
                 new_val = rnd()  # random mutation
                 individual[i] = new_val
-                # random increase/decrease
-                # new_val = individual[i] * random.choice([0.8, 0.9, 1.1, 1.2])
-                # i2 = i
-                # while i2 == i:
-                #     i2 = random.randint(0, self.n_genes - 1)
-                # individual[i], individual[i2] = individual[i2], individual[i]  # random swap
-                # print('after mutation:')
-                # pprint(individual)
                 self.mutations_occurred += 1
 
     def output(self):
@@ -172,11 +127,10 @@ Mutations: {self.mutations_occurred} (prob {self.mut_prob})
 Top fit values / individuals:
 {top_res}
 Max possible fit value: {self.max_possible_fit_value}
-All-time record: {self.all_time_record}
+All-time record: {self.all_time_record} @ generation {self.record_generation}
 Record holder: {self.record_holder}
 '''
-        # print(out_s)
-        file_name = f'fight_ai_gen output {self.comment}.txt'
+        file_name = f'fight_ai_gen output {self.comment} generation_{self.curr_generation}.txt'
         file_path = Path('tests', 'genetic', file_name)
         print(out_s, file=open(file_path, 'w', encoding='utf-8'))
 
@@ -186,11 +140,7 @@ Record holder: {self.record_holder}
         self.n_generations = n_generations
         for i in trange(n_generations):
             self.curr_generation = i
-            # print('generation', i + 1, '/', n_generations)
-            if TRAINING_MODE == 1:
-                self.fitness()
-            else:
-                self.fitness_infighting()
+            self.fitness_function()
             self.selection()
             self.output()
             self.crossover()
@@ -198,15 +148,14 @@ Record holder: {self.record_holder}
 
     def selection(self):
         """Set self.fittest"""
-        # print(time.ctime())
-        # print('starting selection')
         scores = [(val, self.population[i]) for i, val in enumerate(self.fit_values)]
         scores = sorted(scores, reverse=True)
-        self.fittest = [tup[1] for tup in scores][: self.n_top]
-        self.fit_values_sorted = [tup[0] for tup in scores][: self.n_top]
+        self.fittest = [tup[1] for tup in scores][:self.n_top]
+        self.fit_values_sorted = [tup[0] for tup in scores][:self.n_top]
         # print('scores:')
         # pprint(scores)
         if self.fit_values_sorted[0] > self.all_time_record:
             # print('New record!')
             self.all_time_record = self.fit_values_sorted[0]
             self.record_holder = self.fittest[0]
+            self.record_generation = self.curr_generation
