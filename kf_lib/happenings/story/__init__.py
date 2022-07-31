06832525462f752faeaab2b6ebafc56a3853fab0
AlexeyMalafeev <amalafeev@yandex.ru>
2022-07-31 19:08:00 +0300

    refactor: strange dreams story

--- kf_lib/happenings/story/__init__.py
+++ kf_lib/happenings/story/__init__.py
@@ -78,9 +78,7 @@ def get_all_stories() -> List[Type[BaseStory]]:
 
 
 # exp
-DREAM1_EXP = 50
-DREAM2_EXP = 100
-DREAM3_EXP = 200
+
 
 # money
 BRIBE = 100
@@ -91,59 +89,6 @@ OFFICIALS_BODYGUARDS = (3, 5)
 # reputation
 BRIBERY_REP_PENALTY = -5
 RECOVER_TREASURES = 30
-
-
-
-
-
-class StrangeDreamsBaseStory(BaseStory):
-    min_level = 6
-    max_level = 8
-
-    def intro(self):
-        g = self.game
-        g.cls()
-        g.show('Every so often one has some really unusual dreams...')
-        g.pak()
-
-    def reward(self):
-        p = self.player
-        p.add_accompl('Beat Self')
-        p.gain_exp(DREAM3_EXP)
-        p.pak()
-
-    def scene1(self):
-        p = self.player
-        p.write(f'{p.name} has a strange dream...')
-        p.choose_best_norm_wp()
-        ens = fighter_factory.new_opponent(n=4, rand_atts_mode=0)
-        for en in ens:
-            en.arm('chopsticks')
-        if p.spar(ens[0], en_allies=ens[1:]):
-            p.gain_exp(DREAM1_EXP)
-            p.pak()
-
-    def scene2(self):
-        p = self.player
-        p.write(f'{p.name} has a strange dream...')
-        p.pak()
-        en = fighter_factory.new_monster(lv=p.level)
-        en.name = 'Weird ' + en.name
-        if p.spar(en):
-            p.gain_exp(DREAM2_EXP)
-            p.pak()
-
-    def scene3(self):
-        p = self.player
-        p.write(f'{p.name} has a strange dream...')
-        p.pak()
-        en = fighter_factory.copy_fighter(p)
-        en.__class__ = fighter_factory.Fighter
-        if p.spar(en):
-            self.reward()
-
-        # end of the story
-        self.end()
 
 
 class TreasuresBaseStory(BaseStory):
