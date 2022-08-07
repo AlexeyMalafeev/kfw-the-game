2cf8b02c2d90050fafa08c98477f7c9b1fcfc0c7
AlexeyMalafeev <amalafeev@yandex.ru>
2022-08-07 18:39:50 +0300

    feat: more informative warnings in _validators.py

--- kf_lib/utils/_validators.py
+++ kf_lib/utils/_validators.py
@@ -15,11 +15,11 @@ class Validator(ABC):
         return getattr(obj, self.private_name)
 
     def __set__(self, obj, value):
-        value = self._validate(value)
+        value = self._validate(value, obj)
         setattr(obj, self.private_name, value)
 
     @abstractmethod
-    def _validate(self, value):
+    def _validate(self, value, obj):
         pass
 
 
@@ -55,16 +55,23 @@ class BaseNumber(Validator):
     def _raise(self, output_str: Text):
         raise ValidationError(output_str)
 
-    def _validate(self, value):
+    def _validate(self, value, obj):
         if not isinstance(value, self.expected_type):
-            self._act(f'Expected {self.private_name} {value!r} to be {self.expected_type!r}')
+            self._act(
+                f'Expected {self.private_name} {value!r} to be {self.expected_type!r}'
+                f'for {obj}'
+            )
             return self.expected_type(value)
         if self.minvalue is not None and value < self.minvalue:
-            self._act(f'Expected {self.private_name} {value!r} to be at least {self.minvalue!r}')
+            self._act(
+                f'Expected {self.private_name} {value!r} to be at least {self.minvalue!r}'
+                f'for {obj}'
+            )
             return self.minvalue
         if self.maxvalue is not None and value > self.maxvalue:
             self._act(
-                f'Expected {self.private_name} {value!r} to be no more than {self.maxvalue!r}')
+                f'Expected {self.private_name} {value!r} to be no more than {self.maxvalue!r}'
+                f'for {obj}')
             return self.maxvalue
         return value
 
