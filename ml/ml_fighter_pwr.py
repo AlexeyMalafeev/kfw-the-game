4399760c872019008a13c9ca2fa4489c68e7e9a4
AlexeyMalafeev <amalafeev@yandex.ru>
2022-08-14 13:08:26 +0300

    refactor: ml_fighter_pwr.py (minor)

--- ml/ml_fighter_pwr.py
+++ ml/ml_fighter_pwr.py
@@ -1,15 +1,69 @@
+import os
+
 import numpy as np
 import pandas as pd
 
 from kf_lib.actors.fighter_factory import new_fighter, new_prize_fighter
 from kf_lib.fighting.fight import AutoFight
-from kf_lib.mechanics.experience import extract_features
-from kf_lib.utils import rnd, rndint
+from kf_lib.utils import mean, multiply, rnd, rndint
+
 
 np.random.seed(0)
 
 feature_labels_str = 'lv1,lv2,lvrel,att1,att2,attrel,tech1,tech2,techrel,n1,n2,nrel,wp1,wp2,wprel,y'
 feature_labels = feature_labels_str.split(',')
+
+
+# todo refactor extract_features
+def extract_features(side_a, side_b):
+    # side_a.average_lv, side_b.average_lv, lv_relation
+    # side_a.average_atts, side_b.average_atts, att_relation
+    # side_a.average_techs, side_b.average_techs, tech_relation
+    # side_a.n, side_b.n, n_relative
+    # weapons_a, weapons_b, weapons_rel
+    features = []
+
+    # level-related
+    val1 = mean([f.level for f in side_a])
+    features.append(val1)
+    val2 = mean([f.level for f in side_b])
+    features.append(val2)
+    val3 = round(val1 / val2, 2)
+    features.append(val3)
+
+    # att-related
+    val1 = mean([multiply(f.get_att_values_full()) for f in side_a])
+    features.append(val1)
+    val2 = mean([multiply(f.get_att_values_full()) for f in side_b])
+    features.append(val2)
+    val3 = round(val1 / val2, 2)
+    features.append(val3)
+
+    # tech-related
+    val1 = 1 + mean([len(f.techs) for f in side_a])
+    features.append(val1)
+    val2 = 1 + mean([len(f.techs) for f in side_b])
+    features.append(val2)
+    val3 = round(val1 / val2, 2)
+    features.append(val3)
+
+    # crowd
+    val1 = len(side_a)
+    features.append(val1)
+    val2 = len(side_b)
+    features.append(val2)
+    val3 = round(val1 / val2, 2)
+    features.append(val3)
+
+    # weapon
+    val1 = 1 + mean([0.0] + [f.weapon.dfs_bonus + f.weapon.atk_mean for f in side_a if f.weapon])
+    features.append(val1)
+    val2 = 1 + mean([0.0] + [f.weapon.dfs_bonus + f.weapon.atk_mean for f in side_b if f.weapon])
+    features.append(val2)
+    val3 = round(val1 / val2, 2)
+    features.append(val3)
+
+    return features
 
 
 # todo refactor generate_data
