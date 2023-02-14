4c9a0511263872d2db2b28626f3ca0931e64d23e
AlexeyMalafeev <amalafeev@yandex.ru>
2023-02-14 14:40:49 +0300

    feat: parametrize exp system

--- kf_lib/constants/experience.py
+++ kf_lib/constants/experience.py
@@ -1,7 +1,8 @@
 # base constants
+LV1_EXP = 100
+EXP_DIFF_PER_LV = 50
 BASE_FIGHT_EXP = _BASE = 25
 DRAW_EXP_DIVISOR = 2
-EXP_PER_LEVEL = 100
 
 # derived
 ACCOMPL_EXP = round(_BASE * 2.5)
@@ -9,8 +10,8 @@ BOOK_EXP = (round(_BASE * 0.25), _BASE)
 DREAM1_EXP = round(_BASE * 0.5)
 DREAM2_EXP = _BASE
 DREAM3_EXP = round(_BASE * 1.5)
-HOME_TRAINING_EXP = round(_BASE * 0.15)
+HOME_TRAINING_EXP = round(_BASE * 0.1)
 LOSER_EXP = round(_BASE * 0.1)
-MASTER_TRAINING_EXP = round(_BASE * 0.4)
-SCHOOL_TRAINING_EXP = round(_BASE * 0.4)
+MASTER_TRAINING_EXP = round(_BASE * 1)
+SCHOOL_TRAINING_EXP = round(_BASE * 1)
 SPECTATE_FOREIGNER_EXP = (round(_BASE * 0.25), round(_BASE * 0.75))
